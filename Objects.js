bb518774cd3365ea4ea85e8fd5e3972ff5de5f84
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-05-26 13:40:39 +0530

    first question

---
+++ Objects.js
@@ -0,0 +1,24 @@
+// Object Literal
+// const person={
+//     name:"vinod",
+//     age:22,
+//     greet:()=>{
+//         console.log("Hello");
+//     }
+// }
+// console.log(person.name);
+// console.log(person.age)
+// person.greet();
+
+// Object.Create()
+// const personprototype={
+//     greeting:function(){
+//         console.log("Hello"+this.name);
+//     }
+// }
+// const person= Object.create(personprototype)
+// person.name="vinod"
+// person.age=22
+// person.greeting()
+
+// Constructor function
