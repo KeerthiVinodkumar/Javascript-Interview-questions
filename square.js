46f2d15ac39a389970265cf1025d625b39fac629
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-05-28 20:56:58 +0530

    ninecommit

---
+++ square.js
@@ -0,0 +1,9 @@
+function checksumofsquare(num1,num2,num3){
+    if(Math.pow(num1,2)+Math.pow(num2,2)===Math.pow(num3,3)){
+        return true
+        }
+        else{
+            return false
+        }
+}
+checksumofsquare(3,4,5)
