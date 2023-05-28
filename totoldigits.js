7baa5010837960e5fdf63fe1226dacee6eea6283
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-05-28 23:04:50 +0530

    totoldigits

---
+++ totoldigits.js
@@ -0,0 +1,8 @@
+// Find out total digits of a given number
+
+function totoldigits(number){
+    const digits=number.toString()
+    const digitlength=digits.length
+    return digitlength
+}
+console.log(totoldigits(12345));
