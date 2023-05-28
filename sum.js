8fd14bd940c8bbe225f74746b7eff00f519b00c1
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-05-28 20:41:30 +0530

    eightcomit

---
+++ sum.js
@@ -0,0 +1,9 @@
+function checksumoftwonumbers(num1,num2,num3){
+    if(num1+num2===num3){
+        return true
+    }
+    else {
+        return false
+    }
+}
+console.log(checksumoftwonumbers(10,20,30))
